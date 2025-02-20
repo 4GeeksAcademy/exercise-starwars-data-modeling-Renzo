3345c491368c57150978d873b8d474d639854ca1
relo-ga <40379438+relo-ga@users.noreply.github.com>
2025-02-20 03:31:50 +0000

    Starwars data modeling

--- src/models.py
+++ src/models.py
@@ -6,25 +6,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    username: Mapped[str] = mapped_column(nullable=False)
+    firstName: Mapped[str] = mapped_column(nullable=False)
+    lastName: Mapped[str] = mapped_column(nullable=False)
 
+    #address: Mapped["Address"] = relationship(back_populates="person")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favoritos(Base):
+    __tablename__='favoritos'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    usuario_id: Mapped[str] = mapped_column(ForeignKey('usuario.id'), nullable=True)
+    usuario: Mapped["Usuario"] = relationship()
+
+    planetas_id: Mapped[int] = mapped_column(ForeignKey('planetas.id'), nullable=True)
+    planetas: Mapped["Planetas"] = relationship()
+
+    personajes_id: Mapped[int] = mapped_column(ForeignKey('personajes.id'), nullable=True)
+    personajes: Mapped["Personajes"] = relationship()
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    gravity: Mapped[str] = mapped_column()
+    diameter: Mapped[str] = mapped_column()
+    climate: Mapped[str] = mapped_column()
+    terrain: Mapped[str] = mapped_column()
+
+
+class Personajes(Base):
+    __tablename__='personajes'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    height: Mapped[str] = mapped_column()
+    gender: Mapped[str] = mapped_column()
+    birthYear: Mapped[str] = mapped_column()
+    eyeColor: Mapped[str] = mapped_column()
 
     def to_dict(self):
         return {}
